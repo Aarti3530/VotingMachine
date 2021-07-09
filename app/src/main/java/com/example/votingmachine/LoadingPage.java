ddca9cc729c1c9d307b0bee570c26b8862f9edee
aarti <aarti6264744312@gmail.com>
2021-07-09 18:13:50 +0530

    login page

---
+++ app/src/main/java/com/example/votingmachine/LoadingPage.java
@@ -0,0 +1,14 @@
+package com.example.votingmachine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoadingPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loading_page);
+    }
+}

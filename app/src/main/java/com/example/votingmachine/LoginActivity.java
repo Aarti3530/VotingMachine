e672689746b7d8aeafaa31abcdf9669d52e54240
aarti <aarti6264744312@gmail.com>
2021-07-11 17:04:59 +0530

    login UI improved

--- app/src/main/java/com/example/votingmachine/LoginActivity.java
+++ app/src/main/java/com/example/votingmachine/LoginActivity.java
@@ -3,12 +3,29 @@ package com.example.votingmachine;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class LoginActivity extends AppCompatActivity {
-
+    TextInputEditText username,vcnum;
+    Button login,register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        bindViews();
+        clickListener();
+    }
+
+    private void clickListener() {
+
+    }
+
+    private void bindViews() {
+        username = (TextInputEditText) findViewById(R.id.username);
+        vcnum = (TextInputEditText) findViewById(R.id.vcnum);
+        login = findViewById(R.id.login);
+        register = findViewById(R.id.register);
     }
 }
